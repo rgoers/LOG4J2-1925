669ab5d9df42f536c5a2513707f6215cf22e1082
Ralph Goers <rgoers@nextiva.com>
2017-08-22 10:10:16 -0700

    Commit project

---
+++ src/main/java/Processor.java
@@ -0,0 +1,21 @@
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes("org.apache.logging.log4j.core.config.plugins.*")
+public class Processor extends AbstractProcessor {
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latest();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+/**
+ * Example utility class.
+ */
+public class Utils
+{
+    /**
+     * Does stuff.
+     *
+     * @throws Exception if an error occurs.
+     */
+    public static void checkProject1() throws Exception
+    {
+        // empty
+    }
+
+    /**
+     * Does stuff.
+     *
+     * @throws RuntimeException if an error occurs.
+     */
+    public static void checkProject2() throws RuntimeException
+    {
+        // empty
+    }
+
+    /**
+     * Does stuff.
+     *
+     * @throws IOException if an error occurs.
+     */
+    public static void checkProject3() throws IOException
+    {
+        // empty
+    }
+
+    /**
+     * Does nothing.
+     * @param var A dummy variable.
+     * @return 0.
+     * @throws Exception If an error occurs.
+     */
+    public static int someMethod(String var) throws Exception {
+        return 0;
+    }
+}
